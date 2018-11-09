7d43cc7bbd1dce61d49f7752a026cc1a57bf80f6
mfazari <massimofazari@bluewin.ch>
2018-11-09 21:14:58 +0100

    progress

--- app.js
+++ app.js
@@ -1,41 +1,36 @@
-var createError = require('http-errors');
+//express
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+//express
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+//socket.io instance
+var io = require("socket.io")(server);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//view engine
+app.set('view engine', 'ejs');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+//middlewares
+app.use(express.static('public'))
+
+//routes
+
+app.get('/', (req, res) => {
+
+    res.render('index');
+
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+//listen everywhere
+io.on('connection', (socket) => {
+    console.log('New user connected')
 });
 
-module.exports = app;
+
+
+
+//listen on port 3000
+var server = app.listen(3000);
+
