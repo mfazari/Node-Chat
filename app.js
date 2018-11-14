aa45a19f431ee4fa88bd7b119fec7e67a0b7d07e
mfazari <massimofazari@bluewin.ch>
2018-11-14 15:33:35 +0100

    disconnect implemented

--- app.js
+++ app.js
@@ -37,33 +37,19 @@ io.on('connection', function(socket) {
 
     //listen on new_message
     socket.on('new_message', function(data){
-        //broadcast the new message
+        //broadcast the new message to all clients
         io.sockets.emit('new_message', {message : data.message, username : socket.username});
     });
 
     //listen on typing
     socket.on('typing', function(data) {
+        //broadcast the new message to all clients except the one it is being called on
     	socket.broadcast.emit('typing', {username : socket.username})
-    })
-
-
-    /*
-
-    var connectCounter;
-    socket.on('connect', function() { connectCounter++; });
-    socket.on('disconnect', function() { connectCounter--; });
-
-
-    socket.on('user', function(data){
-        $('#num-users').text(data.currentUsers+' users online');
-        var message = data.username;
-        if(data.connected) {
-            message += ' has joined the chat.';
-        } else {
-            message += ' has left the chat.';
-        }
-        $('#messages').append($('<li>').html('<b>'+ message +'<\/b>'));
     });
-*/
+
+    socket.on('disconnect', function(data) {
+        console.log("User disonnected");
+        io.sockets.emit('disconnect', {username : socket.username});
+    });
 
 });
--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,3 @@
-
 $(function(){
    	//make connection
 	var socket = io.connect('http://localhost:3000')
@@ -14,33 +13,42 @@ $(function(){
 	//Emit message
 	send_message.click(function(){
 		socket.emit('new_message', {message : message.val()});
-	})
+	});
 
 	//Listen on new_message
 	socket.on("new_message", function(data) {
+        //sets feedback back to 0 aka message was sent
 		feedback.html('');
+		//sets value back to 0 aka message was sent
 		message.val('');
+		// Adds HTML/EJS code to end of referred ID. In this case "chatroom"
 		chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
-	})
+	});
 
 	//Emit a username
 	send_username.click(function(){
 		socket.emit('change_username', {username : username.val()
 		});
 
-	})
+	});
 
-	//Emit typing
+	//Emit event typing
 	message.bind("keypress", function() {
-		socket.emit('typing')
-	})
+		socket.emit('typing'); //emit event
+	});
 
-	//Listen on typing
+	//Listen on event typing
 	socket.on('typing', function(data) {
 	    //implement that username is required before message
 	    //if(data.username)
-		feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
-	})
+		feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>");
+	});
+
+	//handle a disconnect
+	socket.on('disconnect', function(data){
+        feedback.html('');
+        chatroom.append("<p class='message'>" + data.username + " " + "HAS LEFT ROOM" + "</p>")
+	});
 
 });
 
